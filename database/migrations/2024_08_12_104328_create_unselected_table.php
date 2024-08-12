6e538be8255d6da142a9048783d363e7c07532bb
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-08-12 16:45:01 +0600

    database table update

--- database/migrations/2024_08_12_104328_create_unselected_table.php
+++ database/migrations/2024_08_12_104328_create_unselected_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('unselected', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id'); // Correct column type for foreign key
-            $table->unsignedBigInteger('service_id'); // Correct column type for foreign key
+            $table->unsignedBigInteger('user_id'); 
+            $table->unsignedBigInteger('service_id'); 
             $table->foreign('user_id')->references('id')->on('user');
             $table->foreign('service_id')->references('id')->on('service_name');
             $table->timestamps();
