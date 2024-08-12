927c9c6c96cd268e020aa3089c369569aee48307
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-08-12 16:44:22 +0600

    database create

---
+++ database/migrations/2024_08_12_104155_create_selected_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('selected', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // Correct column type for foreign key
+            $table->unsignedBigInteger('service_id'); // Correct column type for foreign key
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('service_id')->references('id')->on('service_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('selected');
+    }
+};
