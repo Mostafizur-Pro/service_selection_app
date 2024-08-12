7b5eac3f5427466b32d6cd1ec6c9cb7f26423a9b
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-08-12 16:47:14 +0600

    create model

---
+++ app/Models/UserTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTable extends Model
+{
+    use HasFactory;
+    protected $fillable = ['username', 'service_id'];
+
+    public function service()
+    {
+        return $this->belongsTo(ServiceName::class);
+    }
+
+    public function selected()
+    {
+        return $this->hasOne(Selected::class);
+    }
+
+    public function unselected()
+    {
+        return $this->hasOne(Unselected::class);
+    }
+}
